76e3c19c45b134cc5ffb50c2709563d3276347ae
Markov112 <sam00212@laurea.fi>
2026-06-08 21:51:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,22 +37,21 @@ $(document).ready(function () {
       });
     }
 
-   setTimeout(() => {
+    setTimeout(() => {
+      current++;
 
-  current++;
+      if (current < questions.length) {
+        showQuestion();
+        canClick = true;
+      } else {
+        endGame();
+      }
 
-  if (current < questions.length) {
-    showQuestion();
-    canClick = true;
-  } else {
-    endGame();
-  }
-
-}, 1000);
+    }, 1000);
 
   });
 
-
+}); // 👈 TÄMÄ PUUTTUI SUN KOODISTA
 
 function startGame() {
 
@@ -61,6 +60,48 @@ function startGame() {
   score = 0;
   current = 0;
 
+  $("#score").text(score);
+  $("#feedback").text("");
+
+  $("#start-screen").hide();
+  $("#end-screen").hide();
+  $("#quiz-screen").removeClass("d-none");
+
+  axios.get("https://opentdb.com/api.php?amount=10&type=multiple")
+    .then(res => {
+      questions = res.data.results;
+      showQuestion();
+    })
+    .catch(err => console.log(err));
+}
+
+function showQuestion() {
+
+  $("#feedback").text("");
+
+  let q = questions[current];
+
+  let answers = [...q.incorrect_answers, q.correct_answer];
+  answers.sort(() => Math.random() - 0.5);
+
+  $("#question").html(q.question);
+  $("#answers").empty();
+
+  answers.forEach(a => {
+    $("#answers").append(`
+      <button class="btn btn-outline-light w-100 my-2 answer-btn"
+        data-answer="${a}">
+        ${a}
+      </button>
+    `);
+  });
+}
+
+function endGame() {
+  $("#quiz-screen").addClass("d-none");
+  $("#end-screen").removeClass("d-none");
+  $("#final-score").text(score);
+}
   $("#score").text(score);
   $("#feedback").text("");
 
