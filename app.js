1163277eba791cda05f398f6105a5a8c0e76dc29
Markov112 <sam00212@laurea.fi>
2026-06-08 21:16:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,37 +4,98 @@ let score = 0;
 
 $(document).ready(function () {
 
-  $("#start-btn").click(function () {
+  $("#start-btn").on("click", function () {
     startGame();
   });
 
-  $("#restart-btn").click(function () {
+  $("#restart-btn").on("click", function () {
     location.reload();
   });
 
-  // GLOBAL click handler (IMPORTANT FIX)
+  // turvallinen click handler
   $(document).on("click", ".answer-btn", function () {
-    let selected = $(this).data("answer");
-    let correct = questions[currentQuestion].correct_answer;
-
-    checkAnswer(selected, correct);
+    const selected = $(this).data("answer");
+    checkAnswer(selected);
   });
 
 });
 
 function startGame() {
-  $("#start-screen").hide();
-  $("#quiz-screen").removeClass("d-none");
 
   score = 0;
   currentQuestion = 0;
 
   $("#score").text(score);
 
+  $("#start-screen").hide();
+  $("#end-screen").hide();
+  $("#quiz-screen").removeClass("d-none");
+
   axios.get("https://opentdb.com/api.php?amount=10&type=multiple")
-    .then(response => {
-      questions = response.data.results;
+    .then(res => {
+      questions = res.data.results;
       showQuestion();
+    })
+    .catch(err => {
+      console.log("API error", err);
+    });
+}
+
+function showQuestion() {
+
+  const q = questions[currentQuestion];
+
+  const answers = [...q.incorrect_answers, q.correct_answer];
+
+  answers.sort(() => Math.random() - 0.5);
+
+  $("#question").html(q.question);
+  $("#answers").empty();
+
+  answers.forEach(ans => {
+    $("#answers").append(`
+      <button class="btn btn-outline-light w-100 my-2 answer-btn"
+        data-answer="${ans}">
+        ${ans}
+      </button>
+    `);
+  });
+}
+
+function checkAnswer(selected) {
+
+  const correct = questions[currentQuestion].correct_answer;
+
+  const user = decodeHTML(selected);
+  const right = decodeHTML(correct);
+
+  if (user === right) {
+    score++;
+    $("#score").text(score);
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion < questions.length) {
+    showQuestion();
+  } else {
+    endGame();
+  }
+}
+
+function endGame() {
+
+  $("#quiz-screen").addClass("d-none");
+  $("#end-screen").removeClass("d-none");
+
+  $("#final-score").text(score);
+}
+
+function decodeHTML(text) {
+  const txt = document.createElement("textarea");
+  txt.innerHTML = text;
+  return txt.value;
+}      showQuestion();
     })
     .catch(err => {
       console.error("API error:", err);
