739c6673ab6929d0ceb9c0045a60c1aa807ad96b
Markov112 <sam00212@laurea.fi>
2026-06-08 21:12:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-function decodeHTML(html) {
-  let txt = document.createElement("textarea");
-  txt.innerHTML = html;
-  return txt.value;
-}
 let questions = [];
 let currentQuestion = 0;
 let score = 0;
@@ -17,20 +12,37 @@ $(document).ready(function () {
     location.reload();
   });
 
+  // GLOBAL click handler (IMPORTANT FIX)
+  $(document).on("click", ".answer-btn", function () {
+    let selected = $(this).data("answer");
+    let correct = questions[currentQuestion].correct_answer;
+
+    checkAnswer(selected, correct);
+  });
+
 });
 
 function startGame() {
   $("#start-screen").hide();
   $("#quiz-screen").removeClass("d-none");
 
+  score = 0;
+  currentQuestion = 0;
+
+  $("#score").text(score);
+
   axios.get("https://opentdb.com/api.php?amount=10&type=multiple")
     .then(response => {
       questions = response.data.results;
       showQuestion();
+    })
+    .catch(err => {
+      console.error("API error:", err);
     });
 }
 
 function showQuestion() {
+
   let q = questions[currentQuestion];
 
   let answers = [...q.incorrect_answers];
@@ -40,33 +52,32 @@ function showQuestion() {
   answers.sort(() => Math.random() - 0.5);
 
   $("#question").html(q.question);
-
   $("#answers").empty();
 
- answers.forEach(answer => {
-  $("#answers").append(`
-    <button class="btn btn-outline-light w-100 my-1 answer-btn"
-      data-answer="${answer}">
-      ${answer}
-    </button>
-  `);
-});
-
-$(".answer-btn").click(function () {
-  checkAnswer($(this).data("answer"), q.correct_answer);
-});
+  answers.forEach(answer => {
+    $("#answers").append(`
+      <button class="btn btn-outline-light w-100 my-1 answer-btn"
+        data-answer="${answer}">
+        ${answer}
+      </button>
+    `);
+  });
+}
 
 function checkAnswer(selected, correct) {
-console.log("selected:", selected);
-console.log("correct:", correct);
+
   let userAnswer = decodeHTML(selected);
   let correctAnswer = decodeHTML(correct);
+
+  console.log("selected:", userAnswer);
+  console.log("correct:", correctAnswer);
 
   if (userAnswer === correctAnswer) {
     score++;
   }
 
   $("#score").text(score);
+
   currentQuestion++;
 
   if (currentQuestion < questions.length) {
@@ -81,4 +92,12 @@ function endGame() {
   $("#end-screen").removeClass("d-none");
 
   $("#final-score").text(score);
+}
+
+// FIX HTML ENTITIES (IMPORTANT)
+function decodeHTML(html) {
+  let txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value;
+}
 }
